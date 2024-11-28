0d18598311b0673e2354a7a0f12afb0c618d8351
teja <dotnewsteja@gmail.com>
2024-11-28 23:10:08 +0530

    Basic setup

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import astrology
+from databases import Database
 
 app = FastAPI()
 
@@ -9,3 +10,13 @@ app.include_router(astrology.router)
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Astrology API"}
+
+DATABASE_URL = "postgresql://postgres:password@postgres:5432/astrology_db"
+
+database = Database(DATABASE_URL)
+# To be used in FastAPI routes
+async def connect_to_db():
+    await database.connect()
+
+async def disconnect_from_db():
+    await database.disconnect()
